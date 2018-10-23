56508d37596384c1bcdf1a3dab2fa70fa342de0b
wjtk4444 <wjtk4444@gmail.com>
2018-10-24 01:01:05 +0200

    added auxilary method for base64 encoder testing on string inputs

--- lab1_base64/main.cpp
+++ lab1_base64/main.cpp
@@ -1,7 +1,9 @@
 #include<iostream>
 #include<fstream>
+#include<string>
 
 #include"Base64Encoder.hpp"
+#include"Base64Decoder.hpp"
 
 using namespace std;
 
@@ -13,6 +15,47 @@ void printBits(char m)
     cout << endl;
 }
 
+string base64EncodeString(string input)
+{
+    char in[3], out[4];
+    string base64;
+    base64.reserve((input.length() * 4 / 3) & ~3);
+   
+    int i;
+    for(i = 0; i + 2 < input.length(); i+=3)
+    {
+        in[0] = input[i];
+        in[1] = input[i + 1];
+        in[2] = input[i + 2];
+
+        Base64Encoder::encode(in, out); 
+        base64 += out;
+    }
+
+
+    if(i == input.length())
+        return base64;
+
+    int padding = 1;
+    
+    if (i + 1 == input.length())
+    {
+        in[1] = 0;
+        padding = 2;
+    }
+    else if(i + 2 == input.length())
+    {
+        in[1] = input[i+1];
+    }
+
+    in[0] = input[i];
+    in[2] = 0;
+
+    Base64Encoder::encode(in, out, padding); 
+    base64 += out;
+
+    return base64;
+}
 
 int main(int argc, char **argv)
 {
@@ -22,43 +65,12 @@ int main(int argc, char **argv)
         return 0;
     }
 
-    char test[] = "any carnal pleasur";
-    char input[3], output[4];
-    int i = 0, size = sizeof(test) / sizeof(char) - 1;
+    string s = string("any carnal pleasure.");
+    string b64 = base64EncodeString(s);
 
-    for(int i = 0; i < size; i++)
-        cout << test[i];
+    cout << "s  : " << s << endl;
+    cout << "b64: " << b64 << endl;
 
-    cout << endl;
-    
-        int padding = 0;
-    for(int i = 0; i < size; i+=3)
-    {
-
-        input[0] = test[i];
-        
-        if(i + 2 == size){
-            input[2] = 0;
-            padding = 1;
-        }
-        else
-            input[2] = test[i + 2];
-
-
-        if(i + 1 >= size){
-            input[1] = 0;
-            padding = 2;
-        }
-        else
-            input[1] = test[i + 1];
-
-        Base64Encoder::encode(input, output, padding);
-        
-        for(int j = 0; j < 4; j++)
-            cout << output[j];
-    }
-
-    cout << endl;
 
     return 0;
 }
