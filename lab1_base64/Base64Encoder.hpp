7c9b348cfe29e85d420e237c06755206364a8761
wjtk4444 <wjtk4444@gmail.com>
2018-10-23 23:10:07 +0200

    implemented base64 encoder; changed g++ standard to c++17

--- lab1_base64/Base64Encoder.hpp
+++ lab1_base64/Base64Encoder.hpp
@@ -3,28 +3,30 @@
 class Base64Encoder
 {
 public:
-    static void encode(const char (&input)[3], char (&output)[4])
+    static void encode(const char (&input)[3], char (&output)[4], int padding = 0)
     {
-        output[0] = lookupTable[(input[0] >> 2)                  ]; // 6 first bits
-        output[1] = lookupTable[(input[0] << 6) | (input[1] >> 4)]; // last 2 + first 4 bits
-        output[2] = lookupTable[(input[1] << 4) | (input[2] >> 6)]; // last 4 + first 2 bits
-        output[3] = lookupTable[(input[2] << 2)]; // last 6 bits
+        output[0] = lookupTable[(input[0] >> 2)]; // get first 6 bits from input[0]
+        
+        // get last 2 bits from input[0], move them 4 positions to the left
+        // and append first 4 bits from input[1] to the result
+        output[1] = lookupTable[((input[0] & 0x3 ) << 4) | (input[1] >> 4)];
+        
+        // get last 4 bits from input[1], move them 2 positions to the left
+        // and append first 2 bits from input[2] to the result
+        output[2] = padding > 1 ? '=' : lookupTable[((input[1] & 0xF ) << 2) | (input[2] >> 6)];
+        
+        output[3] = padding ? '=' : lookupTable[(input[2] &  0x3F)]; // get last 6 bits from input[2]
     }
 
-
-
-
-
-
-
-
-
-
-
-
 private:
-    static constexpr char lookupTable[64] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '/'};
+    static constexpr char lookupTable[64] = {   
+            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
+            'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+            'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
+            'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f',
+            'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+            'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+            'w', 'x', 'y', 'z', '0', '1', '2', '3',
+            '4', '5', '6', '7', '8', '9', '+', '/',
+        };
 };
-/*
-
-*/
--- lab1_base64/main.cpp
+++ lab1_base64/main.cpp
@@ -1,9 +1,18 @@
 #include<iostream>
 #include<fstream>
 
-#include"Base64Encoder.cpp"
+#include"Base64Encoder.hpp"
 
 using namespace std;
+
+void printBits(char m)
+{
+    for(int i = 7; i >= 0; i--)
+        cout << ((m >> i) & 1);
+
+    cout << endl;
+}
+
 
 int main(int argc, char **argv)
 {
@@ -13,11 +22,41 @@ int main(int argc, char **argv)
         return 0;
     }
 
-    char in[] = {'M', 'a','n'}, out[4];
-    Base64Encoder::encode(in, out);
+    char test[] = "any carnal pleasur";
+    char input[3], output[4];
+    int i = 0, size = sizeof(test) / sizeof(char) - 1;
 
-    for(int i = 0; i < 4; i++)
-        cout << out[i];
+    for(int i = 0; i < size; i++)
+        cout << test[i];
+
+    cout << endl;
+    
+        int padding = 0;
+    for(int i = 0; i < size; i+=3)
+    {
+
+        input[0] = test[i];
+        
+        if(i + 2 == size){
+            input[2] = 0;
+            padding = 1;
+        }
+        else
+            input[2] = test[i + 2];
+
+
+        if(i + 1 >= size){
+            input[1] = 0;
+            padding = 2;
+        }
+        else
+            input[1] = test[i + 1];
+
+        Base64Encoder::encode(input, output, padding);
+        
+        for(int j = 0; j < 4; j++)
+            cout << output[j];
+    }
 
     cout << endl;
 
