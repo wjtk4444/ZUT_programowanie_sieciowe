5edfe611da72dbbca189fa7effb10b9ee1cbd3f3
wjtk4444 <wjtk4444@gmail.com>
2018-11-20 19:08:08 +0100

    fixed typo

--- lab2_POP3/main.cpp
+++ lab2_POP3/main.cpp
@@ -3,32 +3,34 @@
 #include<map>
 #include<fstream>
 
+using namespace std;
+
 int main(int argc, char **argv)
 {
-    std::map<std::string, std::string> settings;
+    map<string, string> settings;
     // defaults
-    settings["adress"  ] = "pop3.wp.pl";
+    settings["address"  ] = "pop3.wp.pl";
     settings["port"    ] = "995"; 
     settings["username"] = "user";
     settings["password"] = "pasword";
     settings["interval"] = "5";
     
-    std::ifstream configFile("App.config");
+    ifstream configFile("App.config");
     if(!configFile.is_open())
     {
-        std::cout << "App.config" << " could not be opened for reading." << std::endl;
+        cout << "App.config" << " could not be opened for reading." << endl;
         return 1;
     }
 
     // read pairs from config file
-    std::string name, value;
+    string name, value;
     while(configFile >> name && configFile >> value)
         if(settings.find(name) != settings.end())
             settings[name] = value;
 
     // print config (debug)
     //for(auto const& pair : settings)
-    //    std::cout << pair.first << " = " << pair.second << std::endl;
+    //    cout << pair.first << " = " << pair.second << endl;
 
     return 0;
 }
