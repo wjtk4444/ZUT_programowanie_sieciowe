69a9c9d93bccebf2a39bf855b777a97dadaeebc0
wjtk4444 <wjtk4444@gmail.com>
2018-11-21 16:45:14 +0100

    major code refactoring; no new features/fixes

--- lab2_POP3/main.cpp
+++ lab2_POP3/main.cpp
@@ -5,53 +5,107 @@
 
 #include"TCPConnection.hpp"
 
+#define SUFFIX  "\r\n"
+
+#define RED     "\x1B[31m"
+#define GREEN   "\x1B[32m"
+#define YELLOW  "\x1B[33m"
+#define BLUE    "\x1B[34m"
+#define MAGENTA "\x1B[35m"
+#define CYANN   "\x1B[36m"
+#define WHITE   "\x1B[37m"
+#define RESET   "\x1B[0m"
+
 using namespace std;
+
+void colorPrint(string color, string message, bool newLine = true)
+{
+    cout << color << message << (newLine ? "\n" : "") << RESET;
+}
+
+void prettyPrintServerResponse(string message)
+{
+    colorPrint(message.find("-ERR") == string::npos ? BLUE : RED,
+        "Server response: " + message, message[message.length() - 1] != '\n');
+}
+
+void prettyPrintSentCommand(string command)
+{
+    colorPrint(GREEN, "Command sent: " + command, command[command.length() - 1] != '\n');
+}
+
+void prettyPrintFailedCommand(string command)
+{
+    colorPrint(RED, "Failed to send command: " + command, command[command.length() - 1] != '\n');
+}
+
+bool sendCommandAndPrintResponse(TCPConnection *connection, string command, string &response)
+{
+    if(!connection->sendData(command))
+    {
+        prettyPrintFailedCommand(command);
+        return false;
+    }
+    prettyPrintSentCommand(command);
+
+    if(!connection->receiveData(response))
+    {
+        colorPrint(RED, "No response from server");
+        return false;
+    }
+    prettyPrintServerResponse(response); 
+    
+    return true;
+}
 
 bool authenticate(TCPConnection *POP3Server, string username, string password)
 {
     string serverResponse;
-    if(!POP3Server->receiveData(serverResponse))
-    {
-        cout << "No response from server." << endl;
-        return false;
-    }
-        cout << serverResponse;
     
-
-    if(!POP3Server->sendData(string("USER ") + username + "\r\n"))
-    {
-        cout << "Failed to send USER command" << endl;
-        return false;
-    }
-
+    // wp.pl hack
     if(!POP3Server->receiveData(serverResponse))
     {
-        cout << "No response from server." << endl;
+        colorPrint(RED, "No response from server");
         return false;
     }
-        cout << serverResponse;
-
-    if(!POP3Server->sendData(string("PASS ") + password + "\r\n"))
-    {
-        cout << "Failed to send USER command" << endl;
+    prettyPrintServerResponse(serverResponse); 
+   
+    // authentication process
+    // USER command
+    if(!sendCommandAndPrintResponse(POP3Server, string("USER ") + username + SUFFIX, serverResponse))
         return false;
-    }
-
-    if(!POP3Server->receiveData(serverResponse))
-    {
-        cout << "No response from server." << endl;
+    // PASS command
+    if(!sendCommandAndPrintResponse(POP3Server, string("PASS ") + password + SUFFIX, serverResponse))
         return false;
-    }
-        cout << serverResponse;
     
+    // "parsing" server response
     if(serverResponse.find("-ERR") != string::npos)
     {
-        cout << "Authentifiction failed" << endl;
+        colorPrint(RED, "Authentifiction failed");
         return false;
     }
 
-    cout << "Authentification successful" << endl;
+    colorPrint(GREEN, "Authentification successful");
     
+    return true;
+}
+
+bool closeConnectionServerside(TCPConnection *POP3Server)
+{
+    string serverResponse;
+    
+    // QUIT command
+    if(!sendCommandAndPrintResponse(POP3Server, string("PASS") + SUFFIX, serverResponse))
+  
+    // "parsing" server response
+    if(serverResponse.find("-ERR") != string::npos)
+    {
+        colorPrint(RED, "Failed to properly close connection");
+        return false;
+    }
+
+    colorPrint(GREEN, "Connection closed: " + serverResponse);
+
     return true;
 }
 
@@ -69,7 +123,7 @@ int main(int argc, char **argv)
     ifstream configFile("App.config");
     if(!configFile.is_open())
     {
-        cout << "App.config" << " could not be opened for reading." << endl;
+        colorPrint(RED, "App.config could not be opened for reading");
         return 1;
     }
 
@@ -80,57 +134,37 @@ int main(int argc, char **argv)
             settings[name] = value;
 
     
-    std::string serverResponse;
+    // create new connection
     TCPConnection POP3Server(settings["address"], atoi(settings["port"].c_str()),
         atoi(settings["useSSL"].c_str()) != 0);
-    
+   
+    // initialize connection
     if(POP3Server.initializeConnection() == true)
-        cout << "Connection initialized successfully." << endl;
+        colorPrint(GREEN, "Connection initialized successfully");
     else
-        cout << "Failed to initialize connection" << endl;
+        colorPrint(RED, "failed to initialize connection");
     
-    authenticate(&POP3Server, settings["username"], settings["password"]);
+    // authenticate on server
+    if(!authenticate(&POP3Server, settings["username"], settings["password"]))
+    {
+        closeConnectionServerside(&POP3Server);
+        POP3Server.closeConnection();
+        return 1;
+    }
+
+    string serverResponse;
     // get list of messages
-    if(!POP3Server.sendData(string("LIST\r\n")))
-    {
-        cout << "Failed to send STAT command" << endl;
-    }
+    if(!POP3Server.sendData(string("LIST") + SUFFIX))
+        colorPrint(RED, "Failed to send STAT command");
 
     if(!POP3Server.receiveData(serverResponse))
-    {
-        cout << "No response from server." << endl;
-    }
-    
-    cout << serverResponse;
+        colorPrint(RED, "No response from server");
 
-    
-    if(!POP3Server.sendData(string("QUIT\r\n")))
-    {
-        cout << "Failed to send QUIT command" << endl;
-        return 1;
-    }
+    prettyPrintServerResponse(serverResponse); 
 
-    if(!POP3Server.receiveData(serverResponse))
-    {
-        cout << "No response from server." << endl;
-        return 1;
-    }
-        cout << serverResponse;
-  
-  if(serverResponse.find("+OK") != string::npos)
-    {
-        cout << serverResponse;
-        return 0;
-    }
-    else
-    {
-        cout << serverResponse;
-        cout << "Failed to properly close connection." << endl;
-        return 1;
-    }
-
+    closeConnectionServerside(&POP3Server);
     POP3Server.closeConnection();
 
-    return 1;
+    return 0;
 }
 
